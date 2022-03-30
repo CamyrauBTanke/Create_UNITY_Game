e22179ac962398041be6d6b1811b2a595dced174
CamyrauBTanke <gunko.vlad.21.09.2001a@gmail.com>
2022-03-30 21:06:56 +0300

    edit scroll

--- game/Assets/Scripts/MainController.cs
+++ game/Assets/Scripts/MainController.cs
@@ -5,29 +5,36 @@ using UnityEngine.SceneManagement;
 public class MainController : MonoBehaviour
 {
     [Header("Volume setting")]
-    [SerializeField] private Text Music_volumeText = null;
-    [SerializeField] private Slider Music_volumeSlider = null;
-    [SerializeField] private Text Dialog_volumeText = null;
-    [SerializeField] private Slider Dialog_volumeSlider = null;
-    [SerializeField] private Text Sound_volumeText = null;
-    [SerializeField] private Slider Sound_volumeSlider = null;
+    [SerializeField] private Text Music_volumeText;
+    [SerializeField] private Slider Music_volumeSlider;
+    [SerializeField] private Text Dialog_volumeText;
+    [SerializeField] private Slider Dialog_volumeSlider;
+    [SerializeField] private Text Sound_volumeText;
+    [SerializeField] private Slider Sound_volumeSlider;
 
-    public void newGame(){
+    [Header("Save Setting")]
+    [SerializeField] private float save_FONT_audio;
+
+    void Start(){
+        GetComponent<AudioSource>();
+        ShowSliderValue();
+    }
+
+    private void newGame(){
         SceneManager.LoadScene("SampleScene");
     }
 
-    public void loadSaveGame(){
+    private void loadSaveGame(){
         
     }
 
-    public void exitGame(){
+    private void exitGame(){
         Application.Quit();
     }
 
-    public void ShowSliderValue()
-    {
-        Music_volumeText.text = Music_volumeSlider.value.ToString() + "%";
-        Dialog_volumeText.text = Dialog_volumeSlider.value.ToString() + "%";
-        Sound_volumeText.text = Sound_volumeSlider.value.ToString() + "%";
+    private void ShowSliderValue(){
+        Music_volumeText.text = Mathf.Round(Music_volumeSlider.value * 100) + "%";
+        Dialog_volumeText.text = Mathf.Round(Dialog_volumeSlider.value * 100) + "%";
+        Sound_volumeText.text = Mathf.Round(Sound_volumeSlider.value * 100) + "%";
     }
 }
