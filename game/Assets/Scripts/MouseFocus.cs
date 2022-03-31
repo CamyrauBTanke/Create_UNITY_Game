7c328accd239a40794b1b638bbf58ea3200aecf0
CamyrauBTanke <gunko.vlad.21.09.2001a@gmail.com>
2022-03-31 21:09:19 +0300

    animation

--- game/Assets/Scripts/MouseFocus.cs
+++ game/Assets/Scripts/MouseFocus.cs
@@ -4,44 +4,23 @@ using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.EventSystems;
 
-public class MouseFocus : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler, IPointerDownHandler, IPointerUpHandler
+public class MouseFocus : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    private bool condition = true;
-
     public void OnPointerEnter(PointerEventData pointerEventData)
     {
-        if(condition == true){
-            Vector3 newRotation = new Vector3(0, 0, 5);
+        Vector3 newRotation = new Vector3(0, 0, 5);
 
-            GetComponentInChildren<Text>().fontSize = 85;
-            GetComponentInChildren<Text>().color = Color.grey;
-            GetComponentInChildren<Text>().transform.eulerAngles = newRotation;
-            condition = false;
-        }
-        else
-            condition = true;
+        GetComponentInChildren<Text>().fontSize = 85;
+        GetComponentInChildren<Text>().color = Color.grey;
+        GetComponentInChildren<Text>().transform.eulerAngles = newRotation;
     }
 
     public void OnPointerExit(PointerEventData pointerEventData)
     {
-        if(condition == false){
-            Vector3 newRotation = new Vector3(0, 0, 0);
+        Vector3 newRotation = new Vector3(0, 0, 0);
 
-            GetComponentInChildren<Text>().fontSize = 70;
-            GetComponentInChildren<Text>().color = Color.white;
-            GetComponentInChildren<Text>().transform.eulerAngles = newRotation;
-            condition = true;
-        }
-        else
-            condition = false;
-    }
-
-    public void OnPointerDown(PointerEventData pointerEventData){
-        GetComponentInChildren<Text>().color = Color.black;
-    }
-
-    public void OnPointerUp(PointerEventData eventData){
-        condition = true;
-        GetComponentInChildren<Text>().color = Color.grey;
+        GetComponentInChildren<Text>().fontSize = 70;
+        GetComponentInChildren<Text>().color = Color.white;
+        GetComponentInChildren<Text>().transform.eulerAngles = newRotation;
     }
 }
